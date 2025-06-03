0445cac0393a8927c4a929e90537e42f6d1aa2b0
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2025-06-03 13:10:14 +0530

    update post api create

--- controller/createPost.controller.js
+++ controller/createPost.controller.js
@@ -94,7 +94,7 @@ export const GetAllPost = async (req, res) => {
   });
 };
 
-export const getposts = async (req,res) => {
+export const getposts = async (req, res) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 0;
@@ -132,19 +132,18 @@ export const getposts = async (req,res) => {
       lastMonthPosts,
     });
   } catch (error) {
-    console.error("posts fetching error ",error);
+    console.error("posts fetching error ", error);
   }
 };
-
 
 export const DeletePost = async (req, res) => {
   try {
     const { userId, isAdmin } = req.user;
     const { userId: paramUserId, postId } = req.params;
-console.log(postId);
+    console.log(postId);
 
     // Authorization check
-    if (!isAdmin || !userId ||!paramUserId) {
+    if (!isAdmin || !userId || !paramUserId) {
       return res.status(403).json({
         message: "You are not authorized to delete this post",
         success: false,
@@ -166,12 +165,84 @@ console.log(postId);
       deletedPost,
       success: true,
     });
-
   } catch (error) {
     console.error("DeletePost Error:", error);
     return res.status(500).json({
       message: "Internal Server Error",
       success: false,
     });
+  }
+};
+
+
+
+//UpdatePost
+export const UpdatePost = async (req, res) => {
+  const postId = req.params.postId;
+  const userId = req.params.userId;
+
+  if (!req.user.isAdmin || req.user.userId != userId) {
+    return res.json({ message: "you are not allowed to update this post!" });
+  }
+    const { title, category, content } = req.body;
+      if (!title || !content || !category) {
+      return res.status(400).json({
+        message: "All fields are must required are required.",
+        success: false,
+      });
+    }
+  // Create slug
+  const slug = title
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+  try {
+    const imageLocalFilePath = req.file?.path;
+   console.log("localfile accesss :",imageLocalFilePath);
+
+    // Upload to Cloudinary
+    const imageurl = await uploadOnCloudinary(imageLocalFilePath);
+    if (!imageurl) {
+      return res.status(500).json({
+        message: "Image upload failed , select new image",
+        success: false,
+      });
+    }
+   console.log("cloud url :",imageurl.url);
+    
+  
+  
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      {
+        $set: {
+          title: title,
+          category: category,
+          slug: slug,
+          image: imageurl.url,
+          content: content,
+          userId: userId,
+        },
+      },
+      { new: true }
+    );
+
+    if (updatedPost) {
+      return res.json({
+        message: "Post updated successfully",
+        updatedPost,
+        success: true,
+      });
+    } else {
+      body;
+      return res.status(404).json({
+        message: "Post not found or upadate",
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.log("error for update post:", error);
   }
 };
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { CreatePost ,GetAllPost, getposts,DeletePost} from "../controller/createPost.controller.js";
+import { CreatePost ,GetAllPost, getposts,DeletePost ,UpdatePost} from "../controller/createPost.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 
 const router = express.Router();
@@ -13,5 +13,7 @@ router.get('/getallpost',getposts);
 //delete post 
 router.delete('/delete-post/:postId/:userId',verifyUser,DeletePost);
 
+//update route
+router.put('/updatepost/:postId/:userId', verifyUser , upload.single("image") , UpdatePost);
 
 export default router;
